317d39aa47d9237deeeb42dbb2b671a55133ed48
Jhsol <133485391+Jhsol3077@users.noreply.github.com>
2023-09-05 21:44:44 +0900

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>환영합니다!</h1>
+        <p>간단한 모바일 웹 앱입니다.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
