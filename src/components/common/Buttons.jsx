2c10ef4c33dc21918b9ddf7ad1fd2be2a4b8e7a3
김하연 <kimhayeon@gimhayeon-ui-MacBookAir.local>
2023-09-09 17:13:25 +0900

    Design: 버튼 컴포넌트 완료

---
+++ src/components/common/Buttons.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const LButton = styled.button`
+  padding: 16px 135px;
+  font-size: var(--text-l);
+  font-weight: 700;
+  border-radius: 100px;
+  background: var(--primary-500);
+  color: var(--white);
+
+  &:disabled {
+    background: var(--gray-300);
+  }
+`;
+
+const MButton = styled(LButton)`
+  padding: 12px 125px;
+`;
+
+const SButton = styled(LButton)`
+  padding: 10px 48px;
+  font-size: var(--text-m);
+`;
+
+const SWhiteButton = styled(SButton)`
+  padding: 9px 47px;
+  border: 1px solid var(--primary-500);
+  color: var(--primary-500);
+  background: var(--white);
+
+  &:disabled {
+    border: 1px solid var(--gray-300);
+    color: var(--gray-300);
+    background: var(--white);
+  }
+`;
+
+export { LButton, MButton, SButton, SWhiteButton };
