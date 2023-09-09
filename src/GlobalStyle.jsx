b9e9e638c5ff8cc4c9d1fdbb3c4570373048bf0b
김하연 <kimhayeon@gimhayeon-ui-MacBookAir.local>
2023-09-09 17:13:01 +0900

    Design: a11y-hidden 클래스 추가

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -61,6 +61,17 @@ const GlobalStyle = createGlobalStyle`
     font-weight: var(--font-medium);
   }
 
+  .a11y-hidden {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+  }
+
   a {
     color: inherit;
     text-decoration: none;
