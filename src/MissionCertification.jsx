c664bf413b1c82875c8ef59b5590a251af543c5b
DESKTOP-P8QQB4J\Joy7 <joyulee77@gmail.com>
2023-09-11 02:37:58 +0900

    Design: 미션페이지 상단, 하단바 완료

--- src/MissionCertification.jsx
+++ src/MissionCertification.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import './MissionCertification.css';
-// import { FaArrowLeft, FaTimes } from 'react-icons/fa'; // 아이콘 라이브러리에서 아이콘을 가져오기.
+import { FaArrowLeft, FaTimes } from 'react-icons/fa'; // 아이콘 라이브러리에서 아이콘을 가져오기.
+
+
 
 function MissionCertification() {
   const handleGoBack = () => {
@@ -15,21 +17,33 @@ function MissionCertification() {
   };
 
   return (
-    <header className="mission-header">
-      <div className="mission-header-content">
-        <button className="icon-button" onClick={handleGoBack}>
-          {/* 이전 페이지로 가는 아이콘 */}
-          {/* <FaArrowLeft />  */}
-        </button>
-        미션 인증
+    <div>
+      {/* 상단 바 부분 시작!---------------------------------------- */}
+      <header className="mission-header">
+        <div className="mission-header-content">       
+          <button className="icon-button" onClick={handleGoBack}>
+            {/* 이전 페이지로 가는 아이콘 */}
+            <FaArrowLeft />
+          </button>
+        </div>
+        
+        <div className="ms-txt">미션인증</div>
+            
         <button className="icon-button" onClick={handleClose}>
           {/* 화면을 닫는 아이콘 */}
-          {/* <FaTimes /> */}
+          <FaTimes />
         </button>
-      </div>
-    </header>
+      </header>
+      {/* 상단 바 부분 끝!---------------------------------------- */}
+      
+      <footer className="bottom-bar">
+        <button className="upload-button">챌린지 피드에 업로드하기</button>
+      </footer>
+    </div>
   );
+  
 }
+  
 
 export default MissionCertification;
 
