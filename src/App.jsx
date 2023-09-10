0565a962c9add441aefe648991e5f23135494406
김하연 <kimhayeon@gimhayeon-ui-MacBookAir.local>
2023-09-11 02:36:33 +0900

    design: 페이지 추가 및 탭바 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
 import GlobalStyle from './GlobalStyle';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Feed from './pages/feed/Feed';
+import My from './pages/my/My';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/feed" element={<Feed />}></Route>
+          <Route path="/my" element={<My />}></Route>
         </Routes>
       </BrowserRouter>
     </>
