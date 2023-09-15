1a188884c33c9cd09636c656b0955c0d25cbdb79
kimhayeon <k.hayeon00@gmail.com>
2023-09-15 15:51:18 +0900

    docs: 파일 정리

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import GlobalStyle from './GlobalStyle';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import MissionCertification from './MissionCertification';
-import Join from './join'; // 공백을 제거하여 올바른 파일 경로로 임포트합니다.
+import Join from './pages/join/Join';
 
 function App() {
   return (
