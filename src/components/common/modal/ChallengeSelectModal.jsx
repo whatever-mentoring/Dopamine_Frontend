e8124de8fe757f758e8a893cc9d36e228e5aa54e
kimhayeon <k.hayeon00@gmail.com>
2023-09-19 14:41:03 +0900

    refactor: 컴포넌트 재사용

--- src/components/common/modal/ChallengeSelectModal.jsx
+++ src/components/common/modal/ChallengeSelectModal.jsx
@@ -1,7 +1,7 @@
 import BottomModal from './BottomModal';
-import { useNavigate } from 'react-router-dom';
 import { useContext, useState } from 'react';
 import { ChallengeContext } from '../../../context/ChallengeContext';
+import ProofModal from './ProofModal';
 
 const ChallengeSelectModal = ({ setIsModalOpen }) => {
   const { challengeList, setChallengeToProve, challengeToProve } =
@@ -13,78 +13,23 @@ const ChallengeSelectModal = ({ setIsModalOpen }) => {
     setSelectedChallenge(e.currentTarget.textContent);
   };
 
-  const navigate = useNavigate();
-  const setImg = (e) => {
-    const file = e.target.files[0];
-    if (!/^image\/(jpg|gif|png|jpeg|bmp|tif|heic)$/.test(file.type)) {
-      alert(
-        '이미지 파일 확장자는 jpg, gif, png, jpeg, bmp, tif, heic만 가능합니다.'
-      );
-      return;
-    }
-
-    const reader = new FileReader();
-
-    reader.readAsDataURL(file);
-    console.log(file);
-
-    reader.addEventListener('load', ({ target }) => {
-      const image = new Image();
-      image.src = target.result;
-      image.addEventListener('load', (e) => {
-        console.log(target.result);
-      });
-    });
-  };
-
-  const handleBtn = (e) => {
-    e.currentTarget.children[0].click();
-    // setIsModalOpen(false);
-  };
-
   return (
-    <BottomModal setIsModalOpen={setIsModalOpen}>
-      <p>
-        {selectedChallenge
-          ? '어떤 방법으로 인증해볼까요?'
-          : '어떤 챌린지를 인증하시나요?'}
-      </p>
-      {!selectedChallenge &&
-        challengeList.map((v, i) => {
-          return (
-            <button key={i} onClick={handleSelectBtn}>
-              {v.title}
-            </button>
-          );
-        })}
-      {selectedChallenge && (
-        <>
-          <button onClick={handleBtn}>
-            촬영하기
-            <input
-              type="file"
-              accept="image/*"
-              capture="camera"
-              className="a11y-hidden"
-            />
-          </button>
-          <button onClick={handleBtn}>
-            갤러리에서 선택하기
-            <input
-              type="file"
-              name="avatar"
-              accept="image/png, image/jpeg"
-              multiple
-              className="a11y-hidden"
-              onChange={(e) => {
-                navigate('/feed');
-                setImg(e);
-              }}
-            />
-          </button>
-        </>
+    <>
+      {!selectedChallenge ? (
+        <BottomModal setIsModalOpen={setIsModalOpen}>
+          <p>'어떤 챌린지를 인증하시나요?'</p>
+          {challengeList.map((v, i) => {
+            return (
+              <button key={i} onClick={handleSelectBtn}>
+                {v.title}
+              </button>
+            );
+          })}
+        </BottomModal>
+      ) : (
+        <ProofModal setIsModalOpen={setIsModalOpen} />
       )}
-    </BottomModal>
+    </>
   );
 };
 
