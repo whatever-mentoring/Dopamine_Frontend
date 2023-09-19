c8fa01e483e75d18790b1800936d0b92319006b3
kimhayeon <k.hayeon00@gmail.com>
2023-09-19 12:05:06 +0900

    remove: 파일 제거

--- src/components/common/statusAlert/StatusAlert.jsx
+++ src/components/common/statusAlert/StatusAlert.jsx
@@ -2,7 +2,13 @@ import StyledDiv from './StyledDiv';
 import successTrue from '../../../assets/icons/success-true.svg';
 import successFalse from '../../../assets/icons/success-false.svg';
 
-const StatusAlert = ({ success, message }) => {
+const StatusAlert = ({ success, message, setRenderStatus }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setRenderStatus(false);
+    }, 6000);
+  }, []);
+
   return (
     <StyledDiv
       img={success === 'true' ? successTrue : successFalse}
