ac84a414d66ea6b9313069f9fc0ac5dbf9947f34
kimhayeon <k.hayeon00@gmail.com>
2023-09-24 01:11:59 +0900

    fix: 삭제 시 리로드

--- src/components/common/modal/DeleteFeedModal.jsx
+++ src/components/common/modal/DeleteFeedModal.jsx
@@ -2,9 +2,10 @@ import BottomModal from './BottomModal';
 import { deleteFeed } from '../../../api/feed';
 
 const DeleteFeedModal = ({ setIsModalOpen, feedId }) => {
-  const handleBtn = async (e) => {
+  const handleBtn = async () => {
     try {
       await deleteFeed(feedId);
+      location.reload();
     } catch (error) {
       console.error(error);
       alert('인증글 삭제에 실패했습니다.');
